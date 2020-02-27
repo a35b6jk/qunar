2007ffff4ef57271e3e90afbd7fd4eb3a811e33f
56337 <563376730@qq.com>
2020-02-27 23:46:05 +0800

    add recommend and axios

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-swiper></home-swiper>
-    <home-icons></home-icons>
+    <home-swiper :swiper-list="swiperList"></home-swiper>
+    <home-icons :icons-list="iconsList"></home-icons>
+    <home-recommend :recommend-list="recommendList"></home-recommend>
+    <home-weekend :weekend-list="weekendList"></home-weekend>
   </div>
 
 </template>
@@ -10,16 +12,47 @@
   import HomeHeader from './components/Header.vue';
   import HomeSwiper from './components/Swiper.vue';
   import HomeIcons from './components/Icons';
-  import axios from 'axios'
+  import HomeRecommend from './components/Recommend';
+  import HomeWeekend from './components/Weekend';
+  import axios from 'axios';
   export default {
     name: 'Home',
     components: {
       'HomeHeader': HomeHeader,
       'HomeSwiper': HomeSwiper,
       'home-icons': HomeIcons,
+      'home-recommend': HomeRecommend,
+      'home-weekend': HomeWeekend
     },
-
-
+    data: function() {
+      return {
+        lastCity: '',
+        swiperList: [],
+        iconsList: [],
+        recommendList: [],
+        weekendList: []
+      }
+    },
+    methods: {
+      getHomeInfo: function() {
+        axios.get('/api/index.json')
+          .then(this.getHomeInfoSuccess)
+      },
+      getHomeInfoSuccess: function(res) {
+        if (res.data.ret && res.data.data) {
+          var data = res.data.data;
+          this.swiperList = data.swiperList;
+          this.iconsList = data.iconsList;
+          this.recommendList = data.recommendList;
+          this.weekendList = data.weekendList;
+          console.log(this.iconsList);
+        }
+      }
+    },
+    mounted: function() {
+      this.lastCity = this.curCity;
+      this.getHomeInfo();
+    },
   }
 </script>
 <style scoped>
